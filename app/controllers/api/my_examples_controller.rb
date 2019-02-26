e18fdd4f21a2664f6d8cc95fc3ec4618d084c3ce
Peter Jang <peter.x.jang@gmail.com>
2019-02-25 18:26:52 -0600

    Add fortune example

--- app/controllers/api/my_examples_controller.rb
+++ app/controllers/api/my_examples_controller.rb
@@ -1,2 +1,11 @@
 class Api::MyExamplesController < ApplicationController
+  def fortune_method
+    fortunes = [
+      "You will be rich",
+      "You will be poor",
+      "You will die a horrible death in three days time"
+    ]
+    @fortune = fortunes.sample
+    render "fortune.json.jbuilder"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+  get "/fortune_url" => "api/my_examples#fortune_method"
 end
